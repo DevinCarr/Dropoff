19ba5257b40e3100e456dd21a5c58f8fefd93352
Devin Carr <devin.carr0@gmail.com>
2018-05-08 23:28:28 -0700

    Post a file from the root dropoff route.

--- Dropoff.Server/Controllers/DropoffController.cs
+++ Dropoff.Server/Controllers/DropoffController.cs
@@ -31,73 +31,91 @@ namespace Dropoff.Server.Controllers
                 return BadRequest("Provided empty or invalid file.");
             }
             // Make sure the file upload isn't too large
-            if (Request.ContentLength > 4e8) 
+            if (Request.ContentLength > 4e8)
             {
                 return BadRequest("File too large.");
             }
             string id = Guid.NewGuid().ToString().Split('-').Aggregate("", (t, n) => t + n);
             string file = Path.Combine(Storage, id);
-            
+
             // If there is no key, just output to the file
             if (string.IsNullOrEmpty(key))
             {
                 using (var fileWriter = new FileStream(file, FileMode.CreateNew))
                 {
-                    using (var binaryWriter = new BinaryWriter(fileWriter))
+                    if (Request.HasFormContentType)
                     {
-                        await Request.Body.CopyToAsync(binaryWriter.BaseStream);
+                        using (var stringWriter = new StreamWriter(fileWriter))
+                        {
+                            var payload = Request.Form["payload"];
+                            if (!string.IsNullOrEmpty(payload))
+                            {
+                                await stringWriter.WriteAsync(payload);
+                            }
+                            else
+                            {
+                                return BadRequest("Provided empty or invalid file.");
+                            }
+                        }
                     }
+                    else
+                    {
+                        using (var binaryWriter = new BinaryWriter(fileWriter))
+                        {
+                            await Request.Body.CopyToAsync(binaryWriter.BaseStream);
+                        }
+                    }
+                    }
+                    return Ok(id);
+                }
+
+                string fileIV = file + ".iv";
+                byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+                int keySize = keyBytes.Length * 8; // size in bits
+                using (var aes = new AesManaged())
+                {
+                    // Verify that the key is long enough
+                    var legal = aes.LegalKeySizes.First();
+                    if (!aes.ValidKeySize(keySize))
+                    {
+                        return BadRequest($"Invalid key length: {keySize}. key length bounds: [{legal.MinSize}, {legal.MaxSize}]");
+                    }
+
+                    // Assign the key and generate an IV
+                    aes.Key = keyBytes;
+                    aes.GenerateIV();
+
+                    using (var ivWriter = new FileStream(fileIV, FileMode.CreateNew))
+                    using (var bin = new BinaryWriter(ivWriter))
+                    {
+                        // Write out the IV for the associated file.
+                        bin.Write(aes.IV);
+                    }
+
+                    // Create a decrytor to perform the stream transform.
+                    ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                    // Create the streams used for encryption.
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        using (CryptoStream csEncrypt = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                        {
+                            using (StreamWriter csWriter = new StreamWriter(csEncrypt))
+                            using (StreamReader bodyReader = new StreamReader(Request.Body))
+                            {
+                                await csWriter.WriteAsync(bodyReader.ReadToEnd());
+                            }
+                            using (var fileWriter = new FileStream(file, FileMode.CreateNew))
+                            using (var bin = new BinaryWriter(fileWriter))
+                            {
+                                bin.Write(ms.ToArray());
+                            }
+                        }
+                    }
+
                 }
                 return Ok(id);
             }
-            
-            string fileIV = file + ".iv";
-            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
-            int keySize = keyBytes.Length * 8; // size in bits
-            using (var aes = new AesManaged())
-            {
-                // Verify that the key is long enough
-                var legal = aes.LegalKeySizes.First();
-                if (!aes.ValidKeySize(keySize))
-                {
-                    return BadRequest($"Invalid key length: {keySize}. key length bounds: [{legal.MinSize}, {legal.MaxSize}]");
-                }
-
-                // Assign the key and generate an IV
-                aes.Key = keyBytes;
-                aes.GenerateIV();
-                
-                using (var ivWriter = new FileStream(fileIV, FileMode.CreateNew))
-                using (var bin = new BinaryWriter(ivWriter))
-                {
-                    // Write out the IV for the associated file.
-                    bin.Write(aes.IV);
-                }
-
-                // Create a decrytor to perform the stream transform.
-                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
-
-                // Create the streams used for encryption.
-                using (MemoryStream ms = new MemoryStream())
-                {
-                    using (CryptoStream csEncrypt = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
-                    {
-                        using (StreamWriter csWriter = new StreamWriter(csEncrypt))
-                        using (StreamReader bodyReader = new StreamReader(Request.Body))
-                        {
-                            await csWriter.WriteAsync(bodyReader.ReadToEnd());
-                        }
-                        using (var fileWriter = new FileStream(file, FileMode.CreateNew))
-                        using (var bin = new BinaryWriter(fileWriter))
-                        {
-                            bin.Write(ms.ToArray());
-                        }
-                    }
-                }
-
-            }
-            return Ok(id);
-        }
 
         // GET /
         // Redirect to 00000000000000000000000000000000 file (Readme).
@@ -125,30 +143,31 @@ namespace Dropoff.Server.Controllers
             string type = GetContentType(t);
             try
             {
-				// If the key is empty, return the file as normal (may or may not be encrypted)
-				if (string.IsNullOrEmpty(key))
+                // If the key is empty, return the file as normal (may or may not be encrypted)
+                if (string.IsNullOrEmpty(key))
                 {
-					var fileStream = new FileStream(file, FileMode.Open, FileAccess.Read);
-					return new FileStreamResult(fileStream, type);
+                    var fileStream = new FileStream(file, FileMode.Open, FileAccess.Read);
+                    return new FileStreamResult(fileStream, type);
                 }
                 using (var aes = new AesManaged())
                 {
-					// Make sure the key length is a proper size
-					byte[] keyBytes = Encoding.UTF8.GetBytes(key);
-					if (!aes.ValidKeySize(keyBytes.Length)) {
-						return BadRequest("Invalid key length.");
-					}
-					aes.Key = keyBytes;
+                    // Make sure the key length is a proper size
+                    byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+                    if (!aes.ValidKeySize(keyBytes.Length))
+                    {
+                        return BadRequest("Invalid key length.");
+                    }
+                    aes.Key = keyBytes;
                     using (var fileIVStream = new FileStream(fileIV, FileMode.Open, FileAccess.Read))
                     using (var binReader = new BinaryReader(fileIVStream))
                     {
                         byte[] iv = binReader.ReadBytes(aes.BlockSize / 8);
                         aes.IV = iv;
                     }
-                                   
+
                     // Create a decrytor to perform the stream transform.
                     ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
-                    
+
                     // Create the streams used for decryption.
                     using (var fileStream = new FileStream(file, FileMode.Open, FileAccess.Read))
                     {
@@ -160,16 +179,22 @@ namespace Dropoff.Server.Controllers
                     }
                 }
 
-            } catch (FileNotFoundException) {
+            }
+            catch (FileNotFoundException)
+            {
                 return NotFound("Key entry does not exist.");
-            } catch (Exception) {
+            }
+            catch (Exception)
+            {
                 return BadRequest("An error occured.");
             }
         }
 
         // Provide some shorthands for Content-Type's
-        private string GetContentType(string type) {
-            switch (type) {
+        private string GetContentType(string type)
+        {
+            switch (type)
+            {
                 case "html": return "text/html";
                 case "json": return "application/json";
                 case "raw": return "text/plain";
