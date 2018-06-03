d0e548bf7d0bca9c56dd7b9f1ea8301a5f785c29
Devin Carr <devin.carr0@gmail.com>
2018-06-03 00:45:45 -0700

    Removed more Auth stuff from Server.

--- Dropoff.Server/Startup.cs
+++ Dropoff.Server/Startup.cs
@@ -1,10 +1,6 @@
-﻿using Dropoff.Server.Extensions;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -24,24 +20,6 @@ namespace Dropoff.Server
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRouting(options => options.LowercaseUrls = true);
-            if (!Configuration.GetValue<string>("ASPNETCORE_ENVIRONMENT").Equals("Development"))
-             {
-                //services.AddAuthentication(sharedOptions =>
-                //{
-                //    sharedOptions.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-                //}).AddAzureAdBearer(options => Configuration.Bind("AzureAd", options));
-                //services.AddMvc(config =>
-                //{
-                //    var policy = new AuthorizationPolicyBuilder()
-                //                     .RequireAuthenticatedUser()
-                //                     .Build();
-                //    config.Filters.Add(new AuthorizeFilter(policy));
-                //}).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            }
-            else
-            {
-                services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            }
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
